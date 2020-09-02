263d0da783c5a3e05334c6b533a2a8d4e495019b
Hans <hans@wireflare.com>
2020-09-02 10:10:54 -0700

    removing module.exports on profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,10 @@
 import useSWR from 'swr'
 
-export function Profile({data, error}) {
+// this page, which was a stub to practice things with, needs refactoring
+export default function Profile({data, error}) {
   // const {data, error} = useSWR('/api/user', fetcher)
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
   return <div>hello {data.name}!</div>
 }
-
-module.exports = {Profile}
