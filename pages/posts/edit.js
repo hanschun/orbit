dbd6faa29275b7dc43f3877d7ac5b09d4d0ebb2d
Hans <hans@wireflare.com>
2020-09-02 10:14:55 -0700

    comment all of getStaticProps for /posts/edit

--- pages/posts/edit.js
+++ pages/posts/edit.js
@@ -2,7 +2,7 @@ export default function() {
   return <div>Under development</div>
 }
 
-export async function getStaticProps(context) {
+// export async function getStaticProps(context) {
   // when context.preview === true append '/preview' to the API endpoint
   // to fetch draft data instead of published data.
   // varies based on the CMS system used.
@@ -13,4 +13,4 @@ export async function getStaticProps(context) {
   // Set this as the preview URL on your headless CMS or access manually,
   // and you should be able to see the preview.
   // https://<your-site>/api/preview?secret=<token>&slug=<path>
-}
+// }
