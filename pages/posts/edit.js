af239a53808160b7fad409421cfa630945e164d4
Hans <hans@wireflare.com>
2020-09-02 10:12:39 -0700

    update edit page to remove sample fetch

--- pages/posts/edit.js
+++ pages/posts/edit.js
@@ -6,7 +6,9 @@ export async function getStaticProps(context) {
   // when context.preview === true append '/preview' to the API endpoint
   // to fetch draft data instead of published data.
   // varies based on the CMS system used.
-  const res = await fetch(`https://.../${content.preview ? 'preview' : ''}`)
+
+  // TODO: uncomment and complete when needed
+  // const res = await fetch(`https://.../${content.preview ? 'preview' : ''}`)
 
   // Set this as the preview URL on your headless CMS or access manually,
   // and you should be able to see the preview.
