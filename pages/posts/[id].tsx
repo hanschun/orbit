ec2fa1abb375f0d3661b01c68b58ce39d43b5426
Hans <hans@wireflare.com>
2020-09-07 16:21:05 -0700

    reinstall id page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,44 +1,44 @@
-// import React, {useEffect, useState} from 'react'
-// import Layout from '../../components/layout'
-// import { getAllPostIds, getPostData } from '../../lib/posts'
-// import Head from 'next/head'
-// import Date from '../../components/date'
-// import utilStyles from '../../styles/utils.module.css'
-// import {GetStaticProps, GetStaticPaths, GetServerSideProps} from 'next'
+import React, {useEffect, useState} from 'react'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import {GetStaticProps, GetStaticPaths, GetServerSideProps} from 'next'
 
 
-// export default function Post({postData}) {
-//   const [posts, setPosts] = useState([])
+export default function Post({postData}) {
+  const [posts, setPosts] = useState([])
   
-//   // update to useEffect method using useApolloClient.query here
+  // update to useEffect method using useApolloClient.query here
 
-//   return <Layout>
-//     <Head>
-//       <title>{postData.title}</title>
-//     </Head>
-//     <article>
-//       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-//       <div className={utilStyles.lightText}>
-//         <Date dateString={postData.date} />
-//       </div>
-//       <div dangerouslySetInnerHTML={{ __html: postData.body }} />
-//     </article>
-//   </Layout>
-// }
+  return <Layout>
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+    <article>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.body }} />
+    </article>
+  </Layout>
+}
 
-// export const getStaticPaths: GetStaticPaths = async() => {
-//   const paths = await getAllPostIds()
-//   return {
-//     paths,
-//     fallback: false
-//   }
-// }
+export const getStaticPaths: GetStaticPaths = async() => {
+  const paths = await getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
 
-// export const getStaticProps: GetStaticProps = async ({params}) => {
-//   const postData = await getPostData(params.id, params.date)
-//   return {
-//     props: {
-//       postData
-//     }
-//   }
-// }
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const postData = await getPostData(params.id, params.date)
+  return {
+    props: {
+      postData
+    }
+  }
+}
